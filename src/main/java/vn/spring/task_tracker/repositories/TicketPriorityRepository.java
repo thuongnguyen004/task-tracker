42c220d239e404633436ef5fbef0aa7cea19dad1
thuongnguyen004 <thuong.nguyen@uniteness.one>
2026-08-06 12:11:19 +0700

    feat: add ticket dto and mapper

---
+++ src/main/java/vn/spring/task_tracker/repositories/TicketPriorityRepository.java
@@ -0,0 +1,16 @@
+package vn.spring.task_tracker.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import vn.spring.task_tracker.entities.TicketPriority;
+import vn.spring.task_tracker.entities.TicketStatus;
+
+import java.util.Optional;
+import java.util.UUID;
+
+@Repository
+public interface TicketPriorityRepository extends JpaRepository<TicketPriority, Short> {
+    Optional<TicketPriority> findByName(String name);
+
+    Optional<TicketPriority> findById(short id);
+}
