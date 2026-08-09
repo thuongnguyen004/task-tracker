9020beef46ee5362197530d67c55f9631cf3a991
tinhtran-apa <tinh.tran@uniteness.one>
2026-08-09 20:01:39 +0700

    feat: add ActivityEventCode enum and integrate it into ActivityEventType entity

---
+++ src/main/java/vn/spring/task_tracker/enums/ActivityEventCode.java
@@ -0,0 +1,12 @@
+package vn.spring.task_tracker.enums;
+
+public enum ActivityEventCode {
+    TICKET_CREATED,
+    TICKET_ARCHIVED,
+    STATUS_CHANGED,
+    ASSIGNEE_CHANGED,
+    PRIORITY_CHANGED,
+    TITLE_CHANGED,
+    DESCRIPTION_CHANGED,
+    COMMENT_ADDED
+}
