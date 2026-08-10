3877cbd87b0ec718eefb2b24a83bb3e268d60d8d
tinhtran-apa <tinh.tran@uniteness.one>
2026-08-11 00:11:35 +0700

    feat: add TicketActivity response and mapper classes; introduce ActivityEventTypeMessage for consistent error messaging

--- src/main/java/vn/spring/task_tracker/dtos/responses/TicketActivityResponse.java
+++ src/main/java/vn/spring/task_tracker/dtos/responses/TicketActivityResponse.java
@@ -1,11 +1,9 @@
-package vn.spring.task_tracker.dtos.responses.ticket_activity;
+package vn.spring.task_tracker.dtos.responses;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
-import vn.spring.task_tracker.entities.ActivityEventType;
-import vn.spring.task_tracker.entities.User;
 import vn.spring.task_tracker.enums.ActivityEventCode;
 
 import java.util.UUID;
