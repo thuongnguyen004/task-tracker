f16ad33f6162a9b53803d958a45f2f418fef6b9d
Tran Ka Bun <bun.tran@uniteness.one>
2026-08-13 11:45:09 +0700

    fix(rebase): main into feature/comments #2

--- src/main/java/vn/spring/task_tracker/entities/ActivityEventType.java
+++ src/main/java/vn/spring/task_tracker/entities/ActivityEventType.java
@@ -2,14 +2,14 @@ package vn.spring.task_tracker.entities;
 
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
-import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import vn.spring.task_tracker.enums.ActivityEventCode;
+
 import java.util.List;
 
 @Entity
 @Data
-@Builder
 @Table(
         name = "activity_event_types",
         uniqueConstraints = {
@@ -22,6 +22,10 @@ public class ActivityEventType {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private short id;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false, unique = true, length = 30)
+    private ActivityEventCode code;
 
     @Column(nullable = false, length = 50)
     private String name;
