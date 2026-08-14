2fefe9a5171cd112b6bd64d54f813bfdd9288b82
Bao <baomttd01287@gmail.com>
2026-08-14 09:14:22 +0700

    feat: fix invalid error message

--- src/main/java/vn/spring/task_tracker/exceptions/GlobalExceptionHandler.java
+++ src/main/java/vn/spring/task_tracker/exceptions/GlobalExceptionHandler.java
@@ -103,10 +103,10 @@ public class GlobalExceptionHandler {
     @ExceptionHandler(Exception.class)
     public ResponseEntity<ErrorResponse> handleGeneralException(Exception ex) {
         ErrorResponse error = new ErrorResponse(
-            LocalDateTime.now(),
-            HttpStatus.INTERNAL_SERVER_ERROR.value(),
-            HttpStatus.INTERNAL_SERVER_ERROR.getReasonPhrase(),
-            ex.getMessage()
+                LocalDateTime.now(),
+                HttpStatus.INTERNAL_SERVER_ERROR.value(),
+                HttpStatus.INTERNAL_SERVER_ERROR.getReasonPhrase(),
+                ex.getMessage()
         );
         return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(error);
     }
