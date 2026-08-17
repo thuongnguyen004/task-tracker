6a2359702a23fffae0b8cdb5852065b70dd39eb2
Bao <baomttd01287@gmail.com>
2026-08-17 10:16:45 +0700

    feat: improve RefreshToken service

--- src/main/java/vn/spring/task_tracker/dtos/responses/LoginResponse.java
+++ src/main/java/vn/spring/task_tracker/dtos/responses/LoginResponse.java
@@ -1,14 +1,12 @@
 package vn.spring.task_tracker.dtos.responses;
 
 import lombok.AllArgsConstructor;
-import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import java.util.UUID;
 
 @Data
-@Builder
 @AllArgsConstructor
 @NoArgsConstructor
 public class LoginResponse {
